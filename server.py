75ff24e2f76af3d0c0cb21a9a9611a0c997a0b26
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-07 17:58:50 -0800

    creates new user_details route and html page, edits base.html to include logout button

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def index():
 
     return render_template("homepage.html")
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Add user to session"""
 
@@ -33,31 +33,36 @@ def login():
     password = request.form['password']
 
     # check if email in db
-    try:
-        User.query.filter(User.email == email).one()
 
-        # check if already logged in via session
-        if email in session:
+    try: 
+        user = User.query.filter(User.email == email).one()
 
-            flash("You're already logged in as {}".format(email))
-            return render_template('homepage.html')
+        # flash("You're already logged in as {}".format(email))
+        # return render_template('homepage.html')
+        if user.password == password:
 
-        else:
-            user = User.query.filter(User.email == email).one()
-            if user.password == password:
-
-                session['email'] = email
-                session['password'] = password
+            session['email'] = email
+            session['password'] = password
 
             flash("Successfully logged in")
 
             return render_template("homepage.html")
             # validate pw via pw in database
+        else:
+            flash("Password incorrect.")
+            return render_template("homepage.html")
+
 
     except:
         return redirect("/register")
         # redirect to register
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    del session['email']
+    del session['password']
+    flash("Successfully logged out")
 
+    return render_template("homepage.html")
 
 @app.route('/users')
 def user_list():
@@ -91,6 +96,35 @@ def register_process():
         db.session.commit()
 
     return redirect("/")
+
+@app.route('/users/<user>')
+def show_user_info(user):
+    """Show information about user"""
+
+    # get age, zipcode, and list of movies rated by user from db
+    # user_id = request.args.get('user.user_id')
+    user = User.query.get(user)
+    age = user.age
+    zipcode = user.zipcode
+
+    user_id = user.user_id 
+
+    ratings_list = Rating.query.filter(Rating.user_id == user_id).all()
+    
+    movie_names = []
+    for rating in ratings_list:
+
+        # this query doesn't work yet 
+        movie_name = Movie.query.filter(Movie.rating_id == rating["rating_id"]).all()
+        movie_names.append(movie_name)
+
+
+    return render_template('user_details.html',
+                                user=user,
+                                age=age,
+                                zipcode=zipcode,
+                                ratings=ratings_list,
+                                movies=movie_list)
 
 
 if __name__ == "__main__":
